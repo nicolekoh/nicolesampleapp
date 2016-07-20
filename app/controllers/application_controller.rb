4f6ceb20ce83a7332c36474e29cd6ad6dd235d8f
nicolekoh <niicolekws@gmail.com>
2016-07-20 14:46:17 +0800

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "hello, world!"
+  	render html: "hello, world! This is Nicole's Sample app "
   end 
 end
